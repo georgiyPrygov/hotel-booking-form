2db30db96946f6b4edc835062f38954a30edc000
georgiyprygov <grishka.pirogov@gmail.com>
2025-10-08 16:15:50 +0200

    Update Mirador page background color for improved aesthetics
    - Changed the background color of the Mirador page from white to a light gray (#e2e0da) to enhance visual appeal and user experience.

--- app/mirador/page.tsx
+++ app/mirador/page.tsx
@@ -3,7 +3,7 @@ import { DatesPicker } from "../../containers/DatesPicker/DatesPicker";
 
 export default function MiradorPage() {
   return (
-    <div className="bg-white py-8">
+    <div className="bg-[#e2e0da] py-8">
       <div className="max-w-4xl mx-auto max-w-[648px]">
         <DatesPicker className="mb-8" isMirador />
       </div>
